fb857d0614439b94afcd7b0680ab0e27fc2e77e9
BaeSuHan0822 <forBaeforBarca@gmail.com>
2025-11-13 16:15:53 +0900

    making vector store

--- embeddings/test.py
+++ embeddings/test.py
@@ -1,7 +1,10 @@
 import os
-# from Kanana import Custom_Kanana
+from Kanana import Custom_Kanana
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# model = Custom_Kanana()
+model = Custom_Kanana()
 
 file_path = "../curriculum_db/"
 output_path = "../chroma_db"
@@ -10,4 +13,26 @@ if os.path.exists(output_path) :
 else :
     os.makedirs(output_path)
 files = os.listdir(file_path)
-print(files)
+
+for file in files :
+    loader = TextLoader(file_path + file)
+    loader = loader.load()
+    
+    pages = [page.page_content for page in loader]
+    text = "\n".join(pages)
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 0,
+        separators = [".","?","!","\n"]
+    )
+    
+    sentences = splitter.split_text(text)
+    
+    vector_store = Chroma(
+        collection_name = file.split(".")[0] + "txt",
+        embedding_function = model,
+        persist_directory = output_path,
+    )
+    
+    
